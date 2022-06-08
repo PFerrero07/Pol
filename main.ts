1a74b1f25d705865e00bbb690b8784e70628c94f
PFerrero07 <106739413+PFerrero07@users.noreply.github.com>
2022-06-08 09:47:41 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,61 @@
 game.splash("", "")
 effects.confetti.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(77, 32)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    mySprite = sprites.createProjectileFromSide(img`
+        ...........ccccc66666...........
+        ........ccc4444444444666........
+        ......cc444444444bb4444466......
+        .....cb4444bb4444b5b444444b.....
+        ....eb4444b5b44444b44444444b....
+        ...ebb44444b4444444444b444446...
+        ..eb6bb444444444bb444b5b444446..
+        ..e6bb5b44444444b5b444b44bb44e..
+        .e66b4b4444444444b4444444b5b44e.
+        .e6bb444444444444444444444bb44e.
+        eb66b44444bb444444444444444444be
+        eb66bb444b5b44444444bb44444444be
+        fb666b444bb444444444b5b4444444bf
+        fcb666b44444444444444bb444444bcf
+        .fbb6666b44444444444444444444bf.
+        .efbb66666bb4444444444444444bfe.
+        .86fcbb66666bbb44444444444bcc688
+        8772effcbbbbbbbbbbbbbbbbcfc22778
+        87722222cccccccccccccccc22226678
+        f866622222222222222222222276686f
+        fef866677766667777776667777fffef
+        fbff877768f86777777666776fffffbf
+        fbeffeefffeff7766688effeeeefeb6f
+        f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+        f66ddfffffeeeffeffeeeeeffeedb46e
+        .c66ddd4effffffeeeeeffff4ddb46e.
+        .fc6b4dddddddddddddddddddb444ee.
+        ..ff6bb444444444444444444444ee..
+        ....ffbbbb4444444444444444ee....
+        ......ffebbbbbb44444444eee......
+        .........fffffffcccccee.........
+        ................................
+        `, 50, 50)
+    mySprite.x += randint(0, scene.screenWidth())
+})
