d61b4221e2f225313938deb9585ed1576bc89d02
PFerrero07 <106739413+PFerrero07@users.noreply.github.com>
2022-06-09 11:59:30 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -52,11 +52,71 @@ mySprite = sprites.create(img`
     . . . c c c c c c c c b b . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(78, 104)
+mySprite.setPosition(27, 120)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setStayInScreen(true)
 info.setLife(3)
-game.onUpdateInterval(1000, function () {
+game.onUpdateInterval(1500, function () {
+    asteroide = sprites.createProjectileFromSide(img`
+        ..............bbbbbbb...........
+        ...........bb66663333baa........
+        .........bb3367776333663aa......
+        ........b33333888333389633aa....
+        .......b3333333333333389633aa...
+        ......b34443333333333338633bae..
+        .....b3455433333333334443333ae..
+        ....b33322333dddd3333455233daee.
+        ...b3d333333dd3bbbb33322333dabe.
+        ..b3d333333d3bb33bb33333333da4e.
+        ..bd33333333b33aab3333333223a4ee
+        .b3d3663333b33aab33366332442b4ee
+        .bd3b983333a3aa3333387633ee3b4ee
+        .bd6983333baaa333333387633bb4bee
+        b3d6833333bba333333333863ba44ebe
+        bdd3333333bb3333333333333a44bebe
+        add666633333322333366333ba44bbbe
+        ad67776333332442336983d3a444b4e.
+        add888b333333ee3369833d3a44b44e.
+        add333333333333336833d3a444b4e..
+        a3dd3333344433333dddd3a444b44e..
+        ab33ddd325543333dd33aa444b44e...
+        .eabb3dd32233333baaa4444b44e....
+        .ebabb3d333d33baa444443b44e.....
+        ..ebaab3ddd3aaa4444433b44e......
+        ..eebbaab33a44444333b444e.......
+        ...eeebbaab444b333b4444e........
+        ....ebeeebbbbbbbb4444ee.........
+        .....eebbbb44444444ee...........
+        .......eeebbb444eee.............
+        ..........eeeeee................
+        ................................
+        `, 0, 50)
+    asteroide.x += randint(0, scene.screenWidth())
+    asteroide.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(1500, function () {
+    asteroide = sprites.createProjectileFromSide(img`
+        . . . . . . b b b b . . . . . . 
+        . . . . . . b 4 4 4 b . . . . . 
+        . . . . . . b b 4 4 4 b . . . . 
+        . . . . . b 4 b b b 4 4 b . . . 
+        . . . . b d 5 5 5 4 b 4 4 b . . 
+        . . . . b 3 2 3 5 5 4 e 4 4 b . 
+        . . . b d 2 2 2 5 7 5 4 e 4 4 e 
+        . . . b 5 3 2 3 5 5 5 5 e e e e 
+        . . b d 7 5 5 5 3 2 3 5 5 e e e 
+        . . b 5 5 5 5 5 2 2 2 5 5 d e e 
+        . b 3 2 3 5 7 5 3 2 3 5 d d e 4 
+        . b 2 2 2 5 5 5 5 5 5 d d e 4 . 
+        b d 3 2 d 5 5 5 d d d 4 4 . . . 
+        b 5 5 5 5 d d 4 4 4 4 . . . . . 
+        4 d d d 4 4 4 . . . . . . . . . 
+        4 4 4 4 . . . . . . . . . . . . 
+        `, 0, 50)
+    asteroide.x += randint(0, scene.screenWidth())
+    asteroide.setKind(SpriteKind.Enemy)
+})
+game.onUpdateInterval(1250, function () {
     asteroide = sprites.createProjectileFromSide(img`
         ...........ccccc66666...........
         ........ccc4444444444666........
