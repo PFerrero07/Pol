82e039845780b5849539f514c78346d94f5a3aa4
PFerrero07 <106739413+PFerrero07@users.noreply.github.com>
2022-06-09 11:45:26 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,21 +19,21 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         `, mySprite, 50, 50)
     music.pewPew.play()
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    otherSprite.destroy(effects.fire, 500)
-    scene.cameraShake(4, 500)
-})
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     asteroide.destroy()
     otherSprite.destroy(effects.fire, 500)
     info.changeScoreBy(1)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+})
 let asteroide: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
 game.splash("BENBINGUTS A L'ESPAI", "Apreta A per començar i B per disparar")
-effects.confetti.startScreenEffect()
+effects.confetti.startScreenEffect(500)
 mySprite = sprites.create(img`
     . . . . . . . . . . b 5 b . . . 
     . . . . . . . . . b 5 b . . . . 
